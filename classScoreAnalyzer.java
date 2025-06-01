07724a7e1fc0f5cf9b90e5dc5067265503930ad4
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-05-31 20:28:30 -0400

    Update classScoreAnalyzer.java

--- classScoreAnalyzer.java
+++ classScoreAnalyzer.java
@@ -10,3 +10,4 @@ public class Main{
 
         // Asks the user if they want to process another class
         System.out.print("\nWould you like to process another class? (Y or N): ");
+        input = myScanner.next();
