8b6b02a2ebf550bea74036dd97496bc5d670d0fc
Kamilla  Mamatova <kamillamamatova.km@gmail.com>
2025-05-31 20:27:43 -0400

    Update classScoreAnalyzer.java

--- classScoreAnalyzer.java
+++ classScoreAnalyzer.java
@@ -4,3 +4,5 @@ public class Main{
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         String input;
+
+        // Call the method that processes class scores
